bb33aa5c6fc27ff5ab3ee8b6324e7838a9359207
Marieke Wesselkamp <Marieke_Wesselkamp@phy-10-126-160-17.eduroam-phy.privat>
2021-05-11 15:29:03 +0200

    starting the project

--- code/main.py
+++ code/main.py
@@ -17,3 +17,28 @@ Inter-annual variability of NN-predicted GPP.
 4. BI-VARIATE CORRELATION ANALYSIS FOR X1, X2, X3.
 
 """
+#%% Set working directory
+import os
+print("Current Working Directory " , os.getcwd())
+os.chdir("/Users/Marieke_Wesselkamp/Documents/Projects/Intern_MPI_Jena") 
+print("Current Working Directory " , os.getcwd())
+
+#%% Set system path
+import sys
+print(sys.path)
+sys.path.append('/Users/Marieke_Wesselkamp/Documents/Projects/Intern_MPI_Jena/code')
+
+#%% Import packages
+import preprocessing
+
+#%%
+X, Y, Y_Preles = preprocessing.load_data()
+X = preprocessing.modify_data(X)
+X = preprocessing.subset_data(X)
+X = preprocessing.standardize_data(X)
+#%% Split data 
+X_P1 , Y_P1, Y_Preles_P1 = preprocessing.split_data(X, Y, Y_Preles,
+                                                    years = [2000, 2001, 2002, 2003, 2004, 2005])
+X_P2 , Y_P2, Y_Preles_P2 = preprocessing.split_data(X, Y, Y_Preles,
+                                                    years = [2006, 2007, 2008, 2009, 2010, 2011])
+
